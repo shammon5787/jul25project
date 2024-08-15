c02893004353864050cd1294c588e1d5617556b0
shammon5787 <sadam.bsit@gmail.com>
2024-08-15 20:41:53 +0500

    moveto gt

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Success from './Pages/Success'
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element ={<Home/>} />
-        <Route path='/success' element ={<Success/>} />
-      </Routes>
-      </BrowserRouter>
+      App
     </div>
   )
 }
